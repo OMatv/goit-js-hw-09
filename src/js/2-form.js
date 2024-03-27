11e8a70aa1daf87137570b350fb3ecabfd72c865
OMatv <149325870+OMatv@users.noreply.github.com>
2024-03-27 15:52:12 +0200

    9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,34 @@
-console.log('Form');
+const form = document.querySelector('.feedback-form');
+
+window.addEventListener('DOMContentLoaded', () => {
+  const savedState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedState) {
+    form.email.value = savedState.email || '';
+    form.message.value = savedState.message || '';
+  }
+});
+
+form.addEventListener('input', () => {
+  const formData = {
+    email: form.email.value.trim(),
+    message: form.message.value.trim(),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = form.email.value.trim();
+  const message = form.message.value.trim();
+
+  if (email && message) {
+    console.log({ email, message });
+
+    localStorage.removeItem('feedback-form-state');
+
+    form.reset();
+  } else {
+    alert('Please fill in all fields.');
+  }
+});
